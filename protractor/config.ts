e9c270747dacb32ad21cb36c1911bdeda46e8835
Diego Cardenas <diego.cardenas@activeone.co>
2018-03-14 14:08:32 -0500

    Configurando travis
    Borrando config.js y modificando config.ts, merging

--- protractor/config.ts
+++ protractor/config.ts
@@ -1,9 +1,5 @@
 import { browser, Config } from 'protractor';
-<<<<<<< HEAD
 import { reporter } from​ './helpers/reporter';
-=======
-import {reporter } from './helpers/reporter';
->>>>>>> master
 
 export const config: Config = {
 	framework: 'jasmine',
