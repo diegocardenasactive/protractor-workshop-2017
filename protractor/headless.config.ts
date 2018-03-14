0aa0d2a413573e0ac0787bcac9148eccc39d79ac
diegocardenasactive <37119018+diegocardenasactive@users.noreply.github.com>
2018-03-14 09:50:34 -0500

    Project setup (#6)
    * Modulos de node.js
    
    Instalación de modulos de node.js
    
    * setup configuration protractor
    
    setup configuration protractor
    
    * Improve example protractor
    
    * Migracion a typescript
    
    Realización de migración a typescript
    
    * Agregando reporte y desactivando promesas
    
    Agregando reporte de consola y desactivando manejador de promesas de selenium
    
    * Travis
    
    Configurando travis para protractor

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -1,15 +1,17 @@
 import { browser, Config } from 'protractor';
-<<<<<<< HEAD
 import { reporter } from​ './helpers/reporter';
-=======
-import {reporter } from './helpers/reporter';
->>>>>>> master
 
 export const config: Config = {
 	framework: 'jasmine',
 	SELENIUM_PROMISE_MANAGER: false,
 	specs: ['../test/google.spec.js'],
 	noGlobals: true,
+	capabilities: {
+		browserName: 'chrome',
+		chromeOptions: {
+			args: ['--headless', '--disable-gpu', '--window-size=800,600']
+		}
+	}, 
 	onPrepare: () => {
 		browser.ignoreSynchronization = true;
 		reporter();
--- test/google.spec.ts
+++ test/google.spec.ts
@@ -1,3 +1,15 @@
+<<<<<<< HEAD
+import {browser} from 'protractor';
+
+describe('Given a SDET learning protractor', () => {
+	describe('when open google page', () => {
+		beforeEach(async () => {
+			await browser.get('http://www.google.com'); 	
+		});
+		
+		it('then should have a title',async () => {
+			await expect(browser.getTitle()).toEqual('Google');
+=======
 
 import { browser } from 'protractor';
 
@@ -9,6 +21,7 @@ describe('Given a SDET learning protractor', () => {
 		
 		it('then should have a title', ()=> {
 			expect(browser.getTitle()).toEqual('Google');
+>>>>>>> master
 		});
 	});
 });
