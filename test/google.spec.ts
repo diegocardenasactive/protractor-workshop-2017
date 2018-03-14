7e822a2ee80bc11bcd2bb50e58f7ebbb74b5e46a
Diego Cardenas <diego.cardenas@activeone.co>
2018-03-14 10:11:09 -0500

    configurando cosas

--- test/google.spec.ts
+++ test/google.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {browser} from 'protractor';
 
 describe('Given a SDET learning protractor', () => {
@@ -9,19 +8,6 @@ describe('Given a SDET learning protractor', () => {
 		
 		it('then should have a title',async () => {
 			await expect(browser.getTitle()).toEqual('Google');
-=======
-
-import { browser } from 'protractor';
-
-describe('Given a SDET learning protractor', () => {
-	describe('when open google page', () => {
-		beforeEach(async() => {
-			browser.get('http://www.google.com'); 	
-		});
-		
-		it('then should have a title', ()=> {
-			expect(browser.getTitle()).toEqual('Google');
->>>>>>> master
 		});
 	});
 });
